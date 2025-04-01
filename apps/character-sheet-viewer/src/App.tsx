2b319496a4c329a4a954afe23c6164b94b983ed6
Kenny Jacobson <kenny.jacobson@gmail.com>
2025-03-31 20:13:43 -0700

    Home page to test

--- apps/character-sheet-viewer/src/App.tsx
+++ apps/character-sheet-viewer/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CharacterSheet } from './pages/CharacterSheet.js';
 import { CharacterSheetAPI } from './pages/CharacterSheetAPI.js';
-
+import { Home } from './pages/Home.js';
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-background">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/character-sheet/:drifterId" element={<CharacterSheet  />} />
           <Route path="/character-sheet-api/:drifterId" element={<CharacterSheetAPI />} />
         </Routes>
---
+++ apps/character-sheet-viewer/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+// Very simple home page for the character sheet viewer
+
+export function Home() {
+  return <div>Home</div>;
+}
+
+
